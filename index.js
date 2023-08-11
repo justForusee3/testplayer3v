c1f7796c416c70548026f406fb1b3c40172e3c2b
justForusee3 <125953355+justForusee3@users.noreply.github.com>
2023-08-11 12:50:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function loadAAMPVideo() {
 
     // Hide the "Play" button and make the video full screen
     var container = document.getElementById("container");
-    container.style.display = "none";
+    // container.style.display = "none";
     video.requestFullscreen().catch(err => {
         console.error('Error attempting to enable full-screen mode:', err);
     });
