efe617d38abe170a0d9853d34862897187e82d90
justForusee3 <125953355+justForusee3@users.noreply.github.com>
2023-08-11 13:58:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,45 @@
 
 
 
+// Single component
+
+var url = "aamps://dash.akamaized.net/dash264/TestCasesMCA/dolby/3/1/ChID_voices_20_128_ddp.mpd";
+var player;
+
+window.onload = function() {
+    player = new AAMPMediaPlayer();
+}
+
+function loadAAMPVideo() {
+    var video = document.getElementById("videoPlayer");
+    video.src = url;
+    video.play();
+    video.style.display = "block";
+
+    // Hide the "Play" button and make the video to be in full screen
+    // var container = document.getElementById("container");
+    // container.style.display = "none";
+    // video.requestFullscreen().catch(error => {
+    //     console.error('Error attempting to enable full-screen mode:', error);
+    // });
+
+    var playButton = document.getElementById('playButton');
+}
+
+playButton.addEventListener("keydown", function(event) {
+    if (event.keyCode === 13) { // Enter key
+        loadAAMPVideo();
+    }
+});
+
+
+
+
+
+
+
+
+
 
 
 
