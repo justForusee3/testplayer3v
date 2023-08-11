107a785aaed401ef461b9e0cada636066cc875cc
justForusee3 <125953355+justForusee3@users.noreply.github.com>
2023-08-11 15:03:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-// var url = "aamps://dash.akamaized.net/dash264/TestCasesMCA/dolby/3/1/ChID_voices_20_128_ddp.mpd";
-// var player;
-
-// window.onload = function() {
-//     player = new AAMPMediaPlayer();
-// }
-
-// function loadAAMPVideo() {
-//     var video = document.getElementById("video");
-//     video.src = url;
-//     video.play();
-//     video.style.display = "block";
-// }
-
-// var playButton = document.getElementById("playButton");
-
-// playButton.addEventListener("keydown", function(event) {
-//     if (event.keyCode === 13) { // Enter key
-//         loadAAMPVideo();
-//     }
-// });
-
-
-
-
-
-
-// Single component
-
 var url = "aamps://dash.akamaized.net/dash264/TestCasesMCA/dolby/3/1/ChID_voices_20_128_ddp.mpd";
 var player;
 
@@ -35,28 +6,57 @@ window.onload = function() {
 }
 
 function loadAAMPVideo() {
-    var video = document.getElementById("videoPlayer");
+    var video = document.getElementById("video");
     video.src = url;
     video.play();
     video.style.display = "block";
-
-    // Hide the "Play" button and make the video to be in full screen
-    // var container = document.getElementById("container");
-    // container.style.display = "none";
-    // video.requestFullscreen().catch(error => {
-    //     console.error('Error attempting to enable full-screen mode:', error);
-    // });
-
-    
 }
 
-var playButton = document.getElementById('playButton');
+var playButton = document.getElementById("playButton");
 
 playButton.addEventListener("keydown", function(event) {
     if (event.keyCode === 13) { // Enter key
         loadAAMPVideo();
     }
 });
+
+
+
+
+
+
+// Single component
+
+// var url = "aamps://dash.akamaized.net/dash264/TestCasesMCA/dolby/3/1/ChID_voices_20_128_ddp.mpd";
+// var player;
+
+// window.onload = function() {
+//     player = new AAMPMediaPlayer();
+// }
+
+// function loadAAMPVideo() {
+//     var video = document.getElementById("videoPlayer");
+//     video.src = url;
+//     video.play();
+//     video.style.display = "block";
+
+//     // Hide the "Play" button and make the video to be in full screen
+//     // var container = document.getElementById("container");
+//     // container.style.display = "none";
+//     // video.requestFullscreen().catch(error => {
+//     //     console.error('Error attempting to enable full-screen mode:', error);
+//     // });
+
+    
+// }
+
+// var playButton = document.getElementById('playButton');
+
+// playButton.addEventListener("keydown", function(event) {
+//     if (event.keyCode === 13) { // Enter key
+//         loadAAMPVideo();
+//     }
+// });
 
 
 
